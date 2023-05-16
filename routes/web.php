65f765a97ee004b9705b8c1f0a300beafb03b140
Anthony cardenas <logicainformatica18@gmail.com>
2023-05-15 20:30:12 -0500

    comentando routes

--- routes/web.php
+++ routes/web.php
@@ -135,9 +135,9 @@ Route::group(['middleware' => ['role:Coordinación']], function () {
    Route::post('studentDestroy',[App\Http\Controllers\StudentController::class, 'destroy']);
    Route::post('studentShow',[App\Http\Controllers\StudentController::class, 'show']);
 
-Route::get('qrcode', function () {
-     return QrCode::size(300)->generate('https://certificados.socialdata-peru.com/');
- });
+// Route::get('qrcode', function () {
+//      return QrCode::size(300)->generate('https://certificados.socialdata-peru.com/');
+//  });
 
 });
 
