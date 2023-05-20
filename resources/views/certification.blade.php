a245005fd22b41c2e5e784446d2c358f91f09959
Anthony cardenas <logicainformatica18@gmail.com>
2023-05-19 21:32:18 -0500

    guardar imagen en storage

--- resources/views/certification.blade.php
+++ resources/views/certification.blade.php
@@ -7,7 +7,7 @@
 
     <meta property="og:title" content="Mi Certificación en : {{$registry_detail->registry->course->description}}"/>
 <meta property="og:description" content="El presente certificado es expedido como reconocimiento alcanzado en el proceso formativo en. {{$registry_detail->registry->course->description}}"/>
-<meta name="image" id="oimage"property="og:image" content="{{asset('storage/certificados/r/edicion/40/certificado.png')}}">
+<meta name="image" id="oimage"property="og:image" content="{{url('storage/certificados/r/edicion/40/certificado.png')}}">
 
     <title>Certificados SDC</title>
     <!-- Tell the browser to be responsive to screen width -->
