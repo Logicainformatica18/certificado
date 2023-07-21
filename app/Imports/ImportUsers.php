d824d992e0cf699db66146a7512da91dbcb3bd55
Anthony cardenas <logicainformatica18@gmail.com>
2023-07-21 17:46:55 -0500

    importacion de usuarios mejorada

--- app/Imports/ImportUsers.php
+++ app/Imports/ImportUsers.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace App\Imports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Hash;
-class ImportUsers implements ToModel,WithHeadingRow
+
+class ImportUsers implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -15,16 +15,26 @@ class ImportUsers implements ToModel,WithHeadingRow
     */
     public function model(array $row)
     {
-       $user=  new User([
-            'dni'    => $row['dni'],
-                'names'     => $row['nombres'],
-            'firstname'    => $row['paterno'],
-            'lastname'    => $row['materno'],
-            'email'    => $row['email'],
-            'password'    => Hash::make($row['password']),
-            'sex'    => substr($row['sexo'],0,1),
-            'cellphone'    => $row['celular'],
+        // Buscar si el correo electrónico ya existe en la base de datos
+        $existingUser = User::where('email', $row['email'])->first();
+
+        // Si el correo electrónico ya está registrado, omitir la importación de este registro
+        if ($existingUser) {
+            return null;
+        }
+
+        // Si el correo electrónico no está registrado, crear un nuevo usuario
+        $user = new User([
+            'dni'       => $row['dni'],
+            'names'     => $row['nombres'],
+            'firstname' => $row['paterno'],
+            'lastname'  => $row['materno'],
+            'email'     => $row['email'],
+            'password'  => Hash::make($row['password']),
+            'sex'       => substr($row['sexo'], 0, 1),
+            'cellphone' => $row['celular'],
         ]);
+
         return $user->assignRole($row['rol']);
     }
 }
