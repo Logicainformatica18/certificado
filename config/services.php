5ae73defe1f0613596256fd1a5985633754294a5
Anthony <logicainformatica18@gmail.com>
2024-06-19 18:39:07 -0500

    corrigiendo gmail acceso

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => 'https://onedigitallcertificado.com/auth/google/callback',
+    'redirect' => 'https://www.onedigitallcertificado.com/auth/google/callback',
 ],
 
 ];
