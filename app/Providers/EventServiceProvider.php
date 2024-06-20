a26340a69ac5d25210f959bce9b3ffb875bc2f15
Anthony <logicainformatica18@gmail.com>
2024-06-20 00:42:25 -0500

    implementando login microsoft

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
+use SocialiteProviders\Microsoft\MicrosoftExtendSocialite;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 class EventServiceProvider extends ServiceProvider
 {
     /**
