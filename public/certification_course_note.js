23fa571ddd129eb8463edffdb51da11b871ac8c3
Anthonycode <logicainformatica18@gmail.com>
2025-01-12 21:23:35 -0500

    add template course

--- public/certification_course_note.js
+++ public/certification_course_note.js
@@ -203,7 +203,7 @@ function certificationDetail(id) {
        ctx.fillStyle = "#0a2262";
    
   
-       ctx.fillText(type, x-70, 730);
+       ctx.fillText(type, x-70, 728);
   
        /////////////////
        ctx.font = "italic 27px Montserrat-Regular";
